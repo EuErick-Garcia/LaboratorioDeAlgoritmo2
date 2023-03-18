6867ad6804efdb84ffb2955ca2ccca9bed77ecd6
EuErick-Garcia <erickgarciarosa1@gmail.com>
2023-03-18 08:39:28 -0300

    Commit inicial

---
+++ aula01/Exer02-Entregar.py
@@ -0,0 +1,18 @@
+
+'''Data de entrega: 22 de mar., 19:00
+1.Ler uma lista
+de 5 números inteiros e mostre cada número juntamente com a sua posição na
+lista;'''
+
+
+'''2.Ler uma lista
+de 10 números reais e mostre-os na ordem inversa''';
+
+
+'''3.Ler uma lista
+de 4 números e mostre o menor e maior número da lista;'''
+
+
+'''4.Crie uma
+programa que crie uma lista de números aleatórios e exiba. O tamanho da lista
+deve ser informado pelo usuário;'''
