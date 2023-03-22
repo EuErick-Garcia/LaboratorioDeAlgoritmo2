959e6c8217fdf5557c8a9f97d80fd45aae36be18
EuErick-Garcia <erickgarciarosa1@gmail.com>
2023-03-22 17:26:29 -0300

    Exercicio de resvisão de lista finalizado!

--- aula01/Exer02-Entregar.py
+++ aula01/Exer02-Entregar.py
@@ -1,5 +1,5 @@
 import os
-
+import random
 def menu():
     print("\033[92mExercises to deliver")
     print("\n\033[92m0 -> \033[94mExit")
@@ -22,6 +22,10 @@ def main():
             exercise_01()
         elif option == 2:
             exercise_02()
+        elif option == 3:
+            exercise_03()
+        elif option == 4:
+            exercise_04()
 
 def exercise_01():
     #Ler uma lista de 5 números inteiros e mostre cada número juntamente com a sua posição na lista
@@ -33,9 +37,9 @@ def exercise_01():
         list_number.append(item)
     for item in range (len(list_number)):
         print(f"{item + 1}°: {list_number[item]}")
-    input("Press enter to continue")
+    input("\033[92mPress enter to continue")
     os.system("cls")
-
+#=======================================================================================================
 def exercise_02():
     #Ler uma lista de 10 números reais e mostre-os na ordem inversa
 
@@ -45,15 +49,37 @@ def exercise_02():
         reversed_values.append(list_number[(index + 1) * - 1])
     print(f'List number: {list_number}')
     print(f'Reversed List: {reversed_values}')
-    input("\nPress enter to continue")
+    input("\n\033[92mPress enter to continue")
     os.system("cls")
-
+#=======================================================================================================
 def exercise_03():
     #Ler uma lista de 4 números e mostre o menor e maior número da lista
     list_number = []
-    for i in list_number:
-        
-
+    for i in range(0, 4):
+        list_number.append(int(input("Enter a number: ")))
+        if i == 0:
+            highest_number = smaller_number = list_number[i]
+        else:
+            if list_number[i] > highest_number:
+                highest_number = list_number[i]
+            if list_number[i] < smaller_number:
+                smaller_number = list_number[i]
+    print(f"\nHighest number: {highest_number}")
+    print(f"smaller number: {smaller_number}")
+    input("\033[92mPress enter to continue")
+    os.system("cls")
+#=======================================================================================================
 def exercise_04():
-    print()
+    #Crie um programa que crie uma lista de números aleatórios e exiba. O tamanho da lista deve ser informado pelo usuário
+    list_number = []
+    amount_number = int(input('amount of index: '))
+
+    for index in range(0, amount_number):
+        number_random = random.randint(0,100)
+        list_number.append(number_random)
+    print("\n\033[94mList random")
+    print(list_number)
+
+    input("\033[92m\nPress enter to continue")
+
 main()
