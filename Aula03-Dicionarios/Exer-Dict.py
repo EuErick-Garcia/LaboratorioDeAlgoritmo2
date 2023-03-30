fa3bcf9183df7c3f738eaae9d37c8635fbabc042
EuErick-Garcia <erickgarciarosa1@gmail.com>
2023-03-29 21:07:30 -0300

    Commit inicial

---
+++ Aula03-Dicionarios/Exer-Dict.py
@@ -0,0 +1,19 @@
+#Testes da Aula....
+'''dicionario = {
+    'nome': 'Erck',
+    'idade': 20,
+    'cidade_natal': 'Restinga Sêca',
+    'teste': 'teste'}
+print(dicionario)
+
+dicionario['cidade_natal'] = input("Informe a sua nova cidade: ")
+print(dicionario)
+dicionario.update(nome='NIck')
+#del dicionario['teste']
+print(dicionario)
+
+dicionario.clear()
+print(dicionario)
+print(dicionario.get('nome', 'nome não incontrado'))'''
+#///////////////////////////////////////////////////////////
+#//////////////////////////////////////////////////////////
