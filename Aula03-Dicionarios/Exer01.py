6670b3e5fb16dfd4a59f82a738166b3de6806ffa
EuErick-Garcia <erickgarciarosa1@gmail.com>
2023-04-05 20:02:53 -0300

    Exercicio 01 feito!

--- Aula03-Dicionarios/Exer01.py
+++ Aula03-Dicionarios/Exer01.py
@@ -1,30 +1,24 @@
 ''' 1. Escreva uma função que recebe uma lista de números e crie um dicionário que seja estruturado
 como sendo o número a chave e a quantidade de vezes que o número está presente o valor.
 Exemplo: [1, 2, 3, 4, 5, 4, 3] => { 1: 1, 2: 1, 3: 2, 4: 2, 5: 1 }'''
+def my_funcion(number_list):
+    number_dict = {}
 
+    for number in number_list:
+        '''Método alterntivo'''
+        #number_dict[number] = number_dict.get(number, 0) + 1
+        if number not in number_dict:
+            number_dict[number] = 1
+        else:
+            number_dict[number] += 1
+            
+    return number_dict
 def main():
-    list_number = list()
-    for i in range (0, 15):
-        number = int(input('Enter a number from 1 to 10: '))
-        list_number.append(number)
-    print(list_number)
 
-    dict_number = {
-        'number_1': 0,
-        'number_2': 0,
-        'number_3': 0,
-        'number_4': 0,
-        'number_5': 0,
-        'number_6': 0,
-        'number_7': 0,
-        'number_8': 0,
-        'number_9' : 0,
-        'number_10' : 0,}
+     my_list = [1, 2, 3, 4, 6, 4, 3, 4]
+     my_variable = my_funcion(my_list)
 
-    for index in list_number:
-
-
-
+     print(my_variable)
 
 main()
 
